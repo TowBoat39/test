0d7b5761941a319689d00600b9a41d4168bb4b75
Павел Жидков <ppavel012100@gmail.com>
2023-06-02 13:03:20 +0200

    1

--- main.java
+++ main.java
@@ -2,4 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.printf("Hello world");
     }
+    public void delete(){
+        System.out.printf("");
+    }
 }
